d15aae391b20656b5c4c8ec254617c0ee97d3044
Fernando00MPR <fer2000226@gmail.com>
2026-06-10 17:32:28 -0600

    fix: evitar scheduler en comandos de inicio

--- paros_app/apps.py
+++ paros_app/apps.py
@@ -1,12 +1,14 @@
 from django.apps import AppConfig
 
+
 class ParosAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'paros_app'
 
     def ready(self):
         import sys
-        if 'migrate' in sys.argv or 'makemigrations' in sys.argv:
+        comandos_skip = ['migrate', 'makemigrations', 'collectstatic', 'compilemessages', 'crear_superusuario']
+        if any(cmd in sys.argv for cmd in comandos_skip):
             return
         try:
             from .scheduler import iniciar_scheduler
