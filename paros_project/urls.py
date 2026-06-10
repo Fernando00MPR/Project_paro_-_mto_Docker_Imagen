39f3bd92038874fab21a444e9fd51b1680c28f18
Fernando00MPR <fer2000226@gmail.com>
2026-06-10 01:34:54 -0600

    fix: servir media con vista directa en produccion

--- paros_project/urls.py
+++ paros_project/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.shortcuts import redirect
 from django.conf.urls.i18n import i18n_patterns
 import django.conf.urls.i18n
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('', include('login_app.urls')),
     path('', include(('paros_app.urls', 'paros'))),
     path('mto/', include(('mto_app.urls', 'mto'))),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
