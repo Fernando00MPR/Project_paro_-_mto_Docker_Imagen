72a301e4cd17950de0bce491055443b67d61b528
Fernando00MPR <fer2000226@gmail.com>
2026-07-02 23:00:49 -0600

    Gestión de Paros de Producción y Mantenimiento

--- paros_app/apps.py
+++ paros_app/apps.py
@@ -7,15 +7,26 @@ class ParosAppConfig(AppConfig):
 
     def ready(self):
         import sys
-        
+
         comandos_skip = ['migrate', 'makemigrations', 'collectstatic', 'compilemessages', 'crear_superusuario']
         if any(cmd in sys.argv for cmd in comandos_skip):
             return
+
+        import os
         import threading
+
+        LOCK = '/tmp/paros_scheduler.lock'
+
         def _start():
+            try:
+                fd = os.open(LOCK, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+                os.close(fd)
+            except FileExistsError:
+                return
             try:
                 from .scheduler import iniciar_scheduler
                 iniciar_scheduler()
             except Exception:
-                pass
+                os.remove(LOCK)
+
         threading.Timer(5.0, _start).start()
